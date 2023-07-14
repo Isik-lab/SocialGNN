204caf611e00544ae62d984313997e8e38c42d3a
Manasi Malik <manasi15146@iiitd.ac.in>
2023-07-14 16:42:40 -0400

    Add files via upload

--- traintest_bootstrapsplits_PHASE_mainset.py
+++ traintest_bootstrapsplits_PHASE_mainset.py
@@ -8,7 +8,7 @@ import argparse
 from distutils.util import strtobool
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--model_name', help="SocialGNN_V/SocialGNN_E/CueBasedLSTM/CueBasedLSTM-Relation", type= str)
+parser.add_argument('--model_name', help="SocialGNN_V/SocialGNN_E/CueBasedLSTM/CueBasedLSTM-Relation/SocialGNN_V_onlyagents/SocialGNN_E_onlyagentedges", type= str)
 parser.add_argument('--context_info', help="True/False", type=lambda x: bool(strtobool(x)), default=False)
 
 args = parser.parse_args()
@@ -49,7 +49,7 @@ if args.model_name == "SocialGNN_V" or args.model_name == "SocialGNN_V_onlyagent
 
     # change parameters for only agent edges
     C = model_config(NUM_NODES = 4, NUM_AGENTS = a, V_SPATIAL_SIZE = 64, E_SPATIAL_SIZE = 64, V_TEMPORAL_SIZE = 16, V_OUTPUT_SIZE = 3, BATCH_SIZE = 20, CLASS_WEIGHTS = [[1.0,2.0,1.0]], LEARNING_RATE = 1e-3, LAMBDA = 0.05 )
-    N_EPOCHS = 3
+    N_EPOCHS = 100
     model = SocialGNN(Videos, C, args.context_info, sample_graph_dicts_list)
     model._initialize_session()
     scores['cross_val'].append(model.cross_validate(5,N_EPOCHS, X_train, y_train, mapping))
@@ -81,7 +81,7 @@ elif args.model_name == "SocialGNN_E" or args.model_name == "SocialGNN_E_onlyage
 
     # change parameters for only agent edges
     C = model_config(NUM_NODES = 4, MAX_EDGES = 12, E_SPATIAL_SIZE = 64, E_TEMPORAL_SIZE = 16, E_OUTPUT_SIZE = 3, BATCH_SIZE = 20, CLASS_WEIGHTS = [[1.0,2.0,1.0]], LEARNING_RATE = 1e-3, LAMBDA = 0.05 )
-    N_EPOCHS = 3
+    N_EPOCHS = 100
     model = SocialGNN_E(Videos, C, args.context_info, sample_graph_dicts_list, ablate = a)
     model._initialize_session()
     scores['cross_val'].append(model.cross_validate(5,N_EPOCHS, X_train, y_train, mapping))
@@ -115,7 +115,7 @@ elif args.model_name == "CueBasedLSTM" or args.model_name == "CueBasedLSTM-Relat
     print("\n\n Bootstrap No.:",split)
 
     C = model_config(FEATURE_SIZE = f_size, V_TEMPORAL_SIZE = 16, V_OUTPUT_SIZE = 3, BATCH_SIZE = 20, CLASS_WEIGHTS = [[1.0,2.0,1.0]], LEARNING_RATE = 1e-3, LAMBDA = 0.05 )
-    N_EPOCHS = 3
+    N_EPOCHS = 100
     model = CueBasedLSTM(Videos, C, args.context_info, explicit_edges = e)
     model._initialize_session()
     scores['cross_val'].append(model.cross_validate(5,N_EPOCHS, X_train, y_train, mapping))
